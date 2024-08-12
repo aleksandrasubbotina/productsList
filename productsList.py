1c129c9ac28c01f163b56813db06d0e2a8b21b09
Aleksandra Subbotina <subbotina.ale@gmail.com>
2024-08-12 14:27:42 +0400

    Added comments for every function

--- productsList.py
+++ productsList.py
@@ -1,15 +1,19 @@
+# Creates a string in JSON format: each element contains unique product ID
 def id_list(count):
     result = ""
     for i in range(count):
         new_element = f'{{"id": {i+1}, "quantity": 1}}, '
         result += new_element
-    #return (f"\{'productsList':\[{result[:-2]}\]\}")
     return result[:-2]
 
+# Creates full request body in JSON format: name of the list and its contents
 def productsList(count):
     return f'{{"productsList":[{id_list(count)}]}}'
 
+# Creates body for another request were only IDs are needed
 def idList(count):
     return f'{{"ids":[{id_list(count)}]}}'
 
-print(productsList(15))
+print(productsList(5))
+
+print(idList(3))
