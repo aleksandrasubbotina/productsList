fe830c9a9ddf258d37347306453c35aa02f43897
Aleksandra Subbotina <subbotina.ale@gmail.com>
2024-08-12 14:57:03 +0400

    Deleted new_element variable.
    Now new content is added directly to result.

--- productsList.py
+++ productsList.py
@@ -1,18 +1,24 @@
 # Creates a string in JSON format: each element contains unique product ID
-def id_list(count):
+def products(count):
     result = ""
     for i in range(count):
-        new_element = f'{{"id": {i+1}, "quantity": 1}}, '
+        result += f'{{"id": {i+1}, "quantity": 1}}, '
+    return result[:-2]
+
+def ids(count):
+    result = ""
+    for i in range(count):
+        new_element = f'"id": {i+1}, '
         result += new_element
     return result[:-2]
 
 # Creates full request body in JSON format: name of the list and its contents
 def productsList(count):
-    return f'{{"productsList":[{id_list(count)}]}}'
+    return f'{{"productsList":[{products(count)}]}}'
 
 # Creates body for another request were only IDs are needed
 def idList(count):
-    return f'{{"ids":[{id_list(count)}]}}'
+    return f'{{"ids":[{ids(count)}]}}'
 
 print(productsList(5))
 
