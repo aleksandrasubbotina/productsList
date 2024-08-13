02bc5063c0fa4cf61e6a0838ac5b9a0ced4d2489
Aleksandra Subbotina <subbotina.ale@gmail.com>
2024-08-13 12:40:15 +0400

    Added productsFromList function.
    It recieves a list of product IDs and returns a string,
    wich is an API request body in JSON format.

--- productsList.py
+++ productsList.py
@@ -1,8 +1,15 @@
 # Creates a string in JSON format: each element contains unique product ID
-def products(count):
+def idAndQuantity(count):
     result = ""
     for i in range(count):
         result += f'{{"id": {i+1}, "quantity": 1}}, '
+    return result[:-2]
+
+#Creates list of products with quantity from a list
+def productsFromList(list):
+    result = ""
+    for i in list:
+        result += f'{{"id": {i}, "quantity": 1}}, '
     return result[:-2]
 
 # Creates a string in JSON format with unique IDs
@@ -12,14 +19,20 @@ def ids(count):
         result += str(i+1) + ", "
     return result[:-2]
 
-# Creates full request body in JSON format: name of the list and its contents
-def productsList(count):
-    return f'{{"productsList":[{products(count)}]}}'
+# Creates full request body with product ids in ascending order and their quantity
+def productsBody(count):
+    return f'{{"productsList":[{idAndQuantity(count)}]}}'
+
+# Creates full request body with product ids from input list and their quantity
+def productsFromListBody(list):
+    return f'{{"productsList":[{productsFromList(list)}]}}'
 
 # Creates body for another request were only IDs are needed
-def idList(count):
+def idListBody(count):
     return f'{{"ids":[{ids(count)}]}}'
 
-print(productsList(5))
+print(productsBody(2))
 
-print(idList(3))
+#print(idListBody(20))
+
+#print(productsFromListBody([6,7,9,10,11,12,14,15,16,18]))
